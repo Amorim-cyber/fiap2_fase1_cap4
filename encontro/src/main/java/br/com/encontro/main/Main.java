195b4a631ece6c2cbe78aeafa029ac27eb892789
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-02-09 20:59:55 -0300

    Atualiza Main.java [1]

--- encontro/src/main/java/br/com/encontro/main/Main.java
+++ encontro/src/main/java/br/com/encontro/main/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Persistence.createEntityManagerFactory("smartcities").createEntityManager();
+		Persistence.createEntityManagerFactory("encontro").createEntityManager();
 
 	}
 
